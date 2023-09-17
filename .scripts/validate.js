a216589b8911974766b391a1910457a56f1954f1
Martyn York <etinquis@gmail.com>
2023-09-17 13:14:34 -0700

    add service schema

--- .scripts/validate.js
+++ .scripts/validate.js
@@ -32,7 +32,8 @@ const main = async function () {
     validateAll("schemas/v1/maps/map-resources.json", "maps/worlds/*/resources.json"),
     validateAll("schemas/v1/maps/terrain.json", "maps/terrains/!(all|all.inline).json"),
     validateAll("schemas/v1/characters/character-title.json", "titles/!(all|all.inline).json"),
-    validateAll("schemas/v1/menus/menu-list.json", "menus/!(all|all.inline).json")
+    validateAll("schemas/v1/menus/menu-list.json", "menus/!(all|all.inline).json"),
+    validateAll("schemas/v1/services/service.json", "./service.json")
   ]);
 };
 
