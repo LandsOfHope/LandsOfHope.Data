554c2698f927890a696fcd5b4e701e3e47bc9608
Martyn York <etinquis@gmail.com>
2023-09-24 12:06:30 -0700

    add type to objects

--- .scripts/validate.js
+++ .scripts/validate.js
@@ -3,8 +3,35 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 const glob = require("glob");
+const readFile = util.promisify(require("fs").readFile);
 
-const schemas = glob.sync("schemas/v1/**/*.json");
+const schemas = glob.sync("schemas/v*/**/*.json");
+const schemasWithOptionalTypeSpecifier = [
+  // for logistical reasons these types should allow $type to be omitted in some circumstances
+
+  // professions ands races were 'exported' by hand and I'm too lazy to fix them all right now.  
+  // If/when proper exports are written, these can be removed
+  'https://data.landsofhope.com/schemas/v1/profession.json',
+  'https://data.landsofhope.com/schemas/v1/race.json',
+
+  // service spec is compatible with package.json; $type would break that compatibility
+  'https://data.landsofhope.com/schemas/v1/services/service.json',
+  'https://data.landsofhope.com/schemas/v1/services/service-dependency.json',
+  // it's just ugly
+  'https://data.landsofhope.com/schemas/v1/localized-string.json',
+
+  // the following map types are ignored primarily due to laziness and not due to techinal reasons
+  // the export needs to be updated to include the types
+  'https://data.landsofhope.com/schemas/v1/maps/map.json',
+  'https://data.landsofhope.com/schemas/v1/maps/terrain.json',
+  'https://data.landsofhope.com/schemas/v1/maps/map-tile.json',
+  // coordinate types are used in maps, so also laziness; could also be argued that {x:1, y:1} is
+  // significantly cleaner than {$type:'/v1/coordinate2', x: 1, y: 1} and jusifies being included
+  // here
+  'https://data.landsofhope.com/schemas/v1/coordinate2.json',
+  'https://data.landsofhope.com/schemas/v1/coordinate3.json',
+
+]
 
 const validateAll = async (schema, fileGlob) => {
   const tmpSchemas = schemas
@@ -20,6 +47,40 @@ const validateAll = async (schema, fileGlob) => {
   ).exitCode;
 };
 
+const validateTestData = (path) => validateAll(`schemas/${path}.json`, `.validation-test-data/${path}.*.json`);
+
+const checkSchemaTypes = async () => {
+  const schemaObjects = Array.from(await Promise.all(schemas.map(schema => readFile(schema, { encoding: 'utf-8' }).then(txt => JSON.parse(txt)))));
+  return schemaObjects.reduce((ret, schema) => {
+    if (schema.type == 'object') {
+      if (schema.oneOf) {
+        // ignore oneOf for the time being
+      }
+      else if (!schema.properties) {
+        if (!(schema.patternProperties || schema.propertyNames)) {
+          console.error(`schema ${schema.$id} is missing properties`);
+          return ret || 1;
+        }
+      }
+      else if (!schema.properties.$type) {
+        if (schemasWithOptionalTypeSpecifier.find(s => s == schema.$id) == undefined) {
+          console.error(`schema ${schema.$id} is missing $type`);
+          return ret || 1;
+        }
+      } else if (!schema.required.find(req => req === "$type")) {
+        if (schemasWithOptionalTypeSpecifier.find(s => s == schema.$id) == undefined) {
+          console.error(`schema ${schema.$id} does not mark $type as required`);
+          return ret || 1;
+        }
+      } else if (!schema.properties.$type.const || (schema.properties.$type.const !== schema.$id.replaceAll('https://data.landsofhope.com/schemas', '').replaceAll('.json', ''))) {
+        console.error(`schema ${schema.$id} has $type.const ${schema.properties.$type.const}`);
+        return ret || 1;
+      }
+    }
+    return ret || 0;
+  }, 0);
+}
+
 const main = async function () {
   return await Promise.all([
     validateAll("schemas/v1/profession.json", "professions/!(all|all.inline).json"),
@@ -33,7 +94,15 @@ const main = async function () {
     validateAll("schemas/v1/maps/terrain.json", "maps/terrains/!(all|all.inline).json"),
     validateAll("schemas/v1/characters/character-title.json", "titles/!(all|all.inline).json"),
     validateAll("schemas/v1/menus/menu-list.json", "menus/!(all|all.inline).json"),
-    validateAll("schemas/v1/services/service.json", "./package.json")
+    validateAll("schemas/v1/services/service.json", "./package.json"),
+
+    validateTestData("v1/characters/appearance-profile"),
+    validateTestData("v1/characters/character-id"),
+    validateTestData("v1/chat/chat-message"),
+    validateTestData("v1/events/chat/messages/chat-message-sent-event"),
+    validateTestData("v1/events/webhook-event"),
+
+    checkSchemaTypes()
   ]);
 };
 
