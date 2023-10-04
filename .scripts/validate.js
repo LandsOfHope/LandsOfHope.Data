107bb15ecb3f7288819171e13fcc61d94b23dbcc
Martyn York <etinquis@gmail.com>
2023-10-04 09:46:06 -0700

    add chat room presence event, rename chat-message-sender to chat-participant

--- .scripts/validate.js
+++ .scripts/validate.js
@@ -101,6 +101,7 @@ const main = async function () {
     validateTestData("v1/characters/appearance-profile"),
     validateTestData("v1/characters/character-id"),
     validateTestData("v1/chat/chat-message"),
+    validateTestData("v1/events/chat/rooms/chat-room-presence-event"),
     validateTestData("v1/events/chat/messages/chat-message-sent-event"),
     validateTestData("v1/events/webhook-event"),
     validateTestData("v1/stats/ranking-list"),
