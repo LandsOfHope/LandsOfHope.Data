58dac416f3d9a9421abed97093578622c288f271
Martyn York <etinquis@gmail.com>
2026-06-03 15:37:59 -0700

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,149 @@
+{
+  description = "LandsOfHope.Data";
+
+  inputs = {
+    nixpkgs.url = "github:etinquis/nixpkgs/nixos-26.05";
+
+    bun2nix = {
+      url = "github:nix-community/bun2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      bun2nix,
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+
+      packages = forSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              bun2nix.overlays.default
+            ];
+          };
+        in
+        rec {
+          default = landsofhope-cdn-data;
+
+          landsofhope-cdn-data = pkgs.stdenv.mkDerivation rec {
+            pname = "landsofhope-cdn-data";
+            version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
+            src = ./.;
+            strictDeps = true;
+            nativeBuildInputs = [
+              pkgs.bun2nix.hook
+              pkgs.bun
+              pkgs.biome
+              pkgs.rsync
+            ];
+            bunRoot = ".scripts";
+            bunDeps = pkgs.bun2nix.fetchBunDeps {
+              bunNix = (
+                pkgs.runCommand "generate-bun-nix" { } ''
+                  ${pkgs.bun2nix}/bin/bun2nix -l ${src}/.scripts/bun.lock -o $out
+                ''
+              );
+            };
+            buildPhase = ''
+              runHook preBuild
+              set -euxo pipefail
+
+              bun .scripts/generate_all_json
+              bun .scripts/generate_id_schemas
+              bun .scripts/generate_other_schemas
+              bun .scripts/validate
+              bun .scripts/compile_schemas
+              (find schemas -name '*.json' | xargs -n1 -P0 bun .scripts/generate_api_ts.ts)
+              bun .scripts/generate_search_indexes
+              biome check api/ --write || true
+
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out
+              rsync --archive --whole-file --one-file-system --safe-links --prune-empty-dirs --exclude-from='.scripts/dist-exclude.txt' ./ $out/
+              runHook postInstall
+            '';
+            dontFixup = true;
+          };
+
+          publish = pkgs.writeShellApplication {
+            name = "publish-landsofhope-cdn-data";
+            runtimeInputs = [
+              pkgs.s3cmd
+            ];
+            text = ''
+              #!/usr/bin/env bash
+              set -euo pipefail
+
+              AWS_ACCESS_KEY_ID=$1
+              shift
+              AWS_SECRET_ACCESS_KEY=$1
+              shift
+              AWS_ENDPOINT_URL=$1
+              shift
+              BUCKET_NAME=$1
+              shift
+
+              cd ${landsofhope-cdn-data}
+              s3cmd sync --no-mime-magic --guess-mime-type \
+                --access_key="$AWS_ACCESS_KEY_ID" \
+                --secret_key="$AWS_SECRET_ACCESS_KEY" \
+                --host="$AWS_ENDPOINT_URL" \
+                --host-bucket="$AWS_ENDPOINT_URL" \
+                --bucket-location=auto \
+                --delete-removed \
+                --delete-after \
+                --no-preserve \
+                --recursive --rexclude-from=/dev/null \
+                ./ "s3://$BUCKET_NAME"
+            '';
+          };
+
+        }
+      );
+
+      overlays = {
+        landsofhope-cdn-data = final: prev: {
+          landsofhope-cdn-data = final.packages.${final.system}.landsofhope-cdn-data;
+        };
+      };
+
+      devShells = forSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              bun2nix.overlays.default
+            ];
+          };
+        in
+        rec {
+          default = devShell;
+
+          devShell = pkgs.mkShellNoCC {
+            buildInputs = with pkgs; [
+              bun
+              biome
+              jq
+              nixfmt
+            ];
+          };
+        }
+      );
+
+    };
+
+}
