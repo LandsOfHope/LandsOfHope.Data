d4945020db665afa7c6097935369d5471024433a
Martyn York <etinquis@gmail.com>
2026-09-01 15:18:17 -0700

    add race search index

--- .scripts/generate_search_indexes.js
+++ .scripts/generate_search_indexes.js
@@ -1,36 +1,36 @@
-import { readFile, writeFile } from "node:fs";
-import { basename, join } from "node:path";
-import { promisify } from "node:util";
-import { sync } from "glob";
+import { readFile, writeFile } from 'node:fs';
+import { basename, join } from 'node:path';
+import { promisify } from 'node:util';
+import { sync } from 'glob';
 
 const readFileAsync = promisify(readFile);
 const writeFileAsync = promisify(writeFile);
 
 const makeMaterialsSearchIndex = async () => {
-	const locales = sync("./locales/*");
+	const locales = sync('./locales/*');
 
 	for (const locale of locales) {
 		const localeName = basename(locale);
 
-		const elasticlunr = (await import("elasticlunr")).default;
-		const stemmer = (await import("lunr-languages/lunr.stemmer.support.js"))
+		const elasticlunr = (await import('elasticlunr')).default;
+		const stemmer = (await import('lunr-languages/lunr.stemmer.support.js'))
 			.default;
 		stemmer(elasticlunr);
-		if (localeName !== "en") {
+		if (localeName !== 'en') {
 			const langStemmer = (await import(`lunr-languages/lunr.${localeName}.js`))
 				.default;
 			langStemmer(elasticlunr);
 		}
 
-		const materials = await readFileAsync(join(locale, "materials.json"));
+		const materials = await readFileAsync(join(locale, 'materials.json'));
 		const index = elasticlunr(function () {
-			if (localeName !== "en") {
+			if (localeName !== 'en') {
 				this.use(elasticlunr[localeName]);
 			}
 
-			this.addField("name");
-			this.addField("level");
-			this.setRef("id");
+			this.addField('name');
+			this.addField('level');
+			this.setRef('id');
 		});
 
 		const materialsJson = JSON.parse(materials);
@@ -45,39 +45,39 @@ const makeMaterialsSearchIndex = async () => {
 		}
 
 		await writeFileAsync(
-			join(locale, "search.materials.index.gen.json"),
+			join(locale, 'search.materials.index.gen.json'),
 			JSON.stringify(index.toJSON()),
 		);
 	}
 };
 
 const makeRecipesSearchIndex = async () => {
-	const locales = sync("./locales/*");
+	const locales = sync('./locales/*');
 
 	for (const locale of locales) {
 		const localeName = basename(locale);
 
-		const elasticlunr = (await import("elasticlunr")).default;
-		const stemmer = (await import("lunr-languages/lunr.stemmer.support.js"))
+		const elasticlunr = (await import('elasticlunr')).default;
+		const stemmer = (await import('lunr-languages/lunr.stemmer.support.js'))
 			.default;
 		stemmer(elasticlunr);
-		if (localeName !== "en") {
+		if (localeName !== 'en') {
 			const langStemmer = (await import(`lunr-languages/lunr.${localeName}.js`))
 				.default;
 			langStemmer(elasticlunr);
 		}
 
-		const recipes = await readFileAsync(join(locale, "crafting-recipes.json"));
-		const skills = JSON.parse(await readFileAsync(join(locale, "skills.json")));
+		const recipes = await readFileAsync(join(locale, 'crafting-recipes.json'));
+		const skills = JSON.parse(await readFileAsync(join(locale, 'skills.json')));
 
 		const index = elasticlunr(function () {
-			if (localeName !== "en") {
+			if (localeName !== 'en') {
 				this.use(elasticlunr[localeName]);
 			}
 
-			this.addField("name");
-			this.addField("skill");
-			this.setRef("id");
+			this.addField('name');
+			this.addField('skill');
+			this.setRef('id');
 		});
 
 		const recipesJson = JSON.parse(recipes);
@@ -92,38 +92,38 @@ const makeRecipesSearchIndex = async () => {
 		}
 
 		writeFileAsync(
-			join(locale, "search.crafting-recipes.index.gen.json"),
+			join(locale, 'search.crafting-recipes.index.gen.json'),
 			JSON.stringify(index.toJSON()),
 		);
 	}
 };
 
 const makeSkillsSearchIndex = async () => {
-	const locales = sync("./locales/*");
+	const locales = sync('./locales/*');
 
 	for (const locale of locales) {
 		const localeName = basename(locale);
 
-		const elasticlunr = (await import("elasticlunr")).default;
-		const stemmer = (await import("lunr-languages/lunr.stemmer.support.js"))
+		const elasticlunr = (await import('elasticlunr')).default;
+		const stemmer = (await import('lunr-languages/lunr.stemmer.support.js'))
 			.default;
 		stemmer(elasticlunr);
-		if (localeName !== "en") {
+		if (localeName !== 'en') {
 			const langStemmer = (await import(`lunr-languages/lunr.${localeName}.js`))
 				.default;
 			langStemmer(elasticlunr);
 		}
 
-		const skills = await readFileAsync(join(locale, "skills.json"));
+		const skills = await readFileAsync(join(locale, 'skills.json'));
 		const index = elasticlunr(function () {
-			if (localeName !== "en") {
+			if (localeName !== 'en') {
 				this.use(elasticlunr[localeName]);
 			}
 
-			this.addField("name");
-			this.addField("desc");
-			this.addField("use");
-			this.setRef("id");
+			this.addField('name');
+			this.addField('desc');
+			this.addField('use');
+			this.setRef('id');
 		});
 
 		const skillsJson = JSON.parse(skills);
@@ -137,37 +137,37 @@ const makeSkillsSearchIndex = async () => {
 		}
 
 		writeFileAsync(
-			join(locale, "search.skills.index.gen.json"),
+			join(locale, 'search.skills.index.gen.json'),
 			JSON.stringify(index.toJSON()),
 		);
 	}
 };
 
 const makeProfessionsSearchIndex = async () => {
-	const locales = sync("./locales/*");
+	const locales = sync('./locales/*');
 
 	for (const locale of locales) {
 		const localeName = basename(locale);
 
-		const elasticlunr = (await import("elasticlunr")).default;
-		const stemmer = (await import("lunr-languages/lunr.stemmer.support.js"))
+		const elasticlunr = (await import('elasticlunr')).default;
+		const stemmer = (await import('lunr-languages/lunr.stemmer.support.js'))
 			.default;
 		stemmer(elasticlunr);
-		if (localeName !== "en") {
+		if (localeName !== 'en') {
 			const langStemmer = (await import(`lunr-languages/lunr.${localeName}.js`))
 				.default;
 			langStemmer(elasticlunr);
 		}
 
-		const professions = await readFileAsync(join(locale, "professions.json"));
+		const professions = await readFileAsync(join(locale, 'professions.json'));
 		const index = elasticlunr(function () {
-			if (localeName !== "en") {
+			if (localeName !== 'en') {
 				this.use(elasticlunr[localeName]);
 			}
 
-			this.addField("name");
-			this.addField("desc");
-			this.setRef("id");
+			this.addField('name');
+			this.addField('desc');
+			this.setRef('id');
 		});
 
 		const professionsJson = JSON.parse(professions);
@@ -180,80 +180,80 @@ const makeProfessionsSearchIndex = async () => {
 		}
 
 		writeFileAsync(
-			join(locale, "search.professions.index.gen.json"),
+			join(locale, 'search.professions.index.gen.json'),
 			JSON.stringify(index.toJSON()),
 		);
 	}
 };
 
 const makeItemsSearchIndex = async () => {
-	const locales = sync("./locales/*");
+	const locales = sync('./locales/*');
 
 	for (const locale of locales) {
 		const localeName = basename(locale);
 
-		const elasticlunr = (await import("elasticlunr")).default;
-		const stemmer = (await import("lunr-languages/lunr.stemmer.support.js"))
+		const elasticlunr = (await import('elasticlunr')).default;
+		const stemmer = (await import('lunr-languages/lunr.stemmer.support.js'))
 			.default;
 		stemmer(elasticlunr);
-		if (localeName !== "en") {
+		if (localeName !== 'en') {
 			const langStemmer = (await import(`lunr-languages/lunr.${localeName}.js`))
 				.default;
 			langStemmer(elasticlunr);
 		}
 
-		const items = await readFileAsync(join(locale, "items.json"));
+		const items = await readFileAsync(join(locale, 'items.json'));
 		const index = elasticlunr(function () {
-			if (localeName !== "en") {
+			if (localeName !== 'en') {
 				this.use(elasticlunr[localeName]);
 			}
 
-			this.addField("name");
-			this.addField("desc");
-			this.setRef("id");
+			this.addField('name');
+			this.addField('desc');
+			this.setRef('id');
 		});
 
 		const itemsJson = JSON.parse(items);
 		for (const [itemId, item] of Object.entries(itemsJson)) {
 			index.addDoc({
+				desc: item.desc,
 				id: `item:${itemId}`,
 				name: item.name,
-				desc: item.desc,
 			});
 		}
 
 		writeFileAsync(
-			join(locale, "search.items.index.gen.json"),
+			join(locale, 'search.items.index.gen.json'),
 			JSON.stringify(index.toJSON()),
 		);
 	}
 };
 
 const makeItemTypesSearchIndex = async () => {
-	const locales = sync("./locales/*");
+	const locales = sync('./locales/*');
 
 	for (const locale of locales) {
 		const localeName = basename(locale);
 
-		const elasticlunr = (await import("elasticlunr")).default;
-		const stemmer = (await import("lunr-languages/lunr.stemmer.support.js"))
+		const elasticlunr = (await import('elasticlunr')).default;
+		const stemmer = (await import('lunr-languages/lunr.stemmer.support.js'))
 			.default;
 		stemmer(elasticlunr);
-		if (localeName !== "en") {
+		if (localeName !== 'en') {
 			const langStemmer = (await import(`lunr-languages/lunr.${localeName}.js`))
 				.default;
 			langStemmer(elasticlunr);
 		}
 
-		const itemTypes = await readFileAsync(join(locale, "item-types.json"));
+		const itemTypes = await readFileAsync(join(locale, 'item-types.json'));
 		const index = elasticlunr(function () {
-			if (localeName !== "en") {
+			if (localeName !== 'en') {
 				this.use(elasticlunr[localeName]);
 			}
 
-			this.addField("name");
-			this.addField("desc");
-			this.setRef("id");
+			this.addField('name');
+			this.addField('desc');
+			this.setRef('id');
 		});
 
 		const itemTypesJson = JSON.parse(itemTypes);
@@ -266,11 +266,57 @@ const makeItemTypesSearchIndex = async () => {
 		}
 
 		writeFileAsync(
-			join(locale, "search.item-types.index.gen.json"),
+			join(locale, 'search.item-types.index.gen.json'),
 			JSON.stringify(index.toJSON()),
 		);
 	}
 };
+
+const makeRacesSearchIndex = async () => {
+	const locales = sync('./locales/*');
+
+	for (const locale of locales) {
+		const localeName = basename(locale);
+
+		const elasticlunr = (await import('elasticlunr')).default;
+		const stemmer = (await import('lunr-languages/lunr.stemmer.support.js'))
+			.default;
+		stemmer(elasticlunr);
+		if (localeName !== 'en') {
+			const langStemmer = (await import(`lunr-languages/lunr.${localeName}.js`))
+				.default;
+			langStemmer(elasticlunr);
+		}
+
+		const races = await readFileAsync(join(locale, 'races.json'));
+		const index = elasticlunr(function () {
+			if (localeName !== 'en') {
+				this.use(elasticlunr[localeName]);
+			}
+
+			this.addField('appr');
+			this.addField('desc');
+			this.addField('gen');
+			this.addField('hist');
+			this.addField('name');
+			this.setRef('id');
+		});
+
+		const racesJson = JSON.parse(races);
+		for (const [raceId, race] of Object.entries(racesJson)) {
+			index.addDoc({
+				...race,
+				id: `race:${raceId}`,
+			});
+		}
+
+		writeFileAsync(
+			join(locale, 'search.races.index.gen.json'),
+			JSON.stringify(index.toJSON()),
+		);
+	}
+}
+
 
 const main = async () =>
 	Promise.all([
@@ -280,6 +326,7 @@ const main = async () =>
 		makeProfessionsSearchIndex(),
 		makeItemsSearchIndex(),
 		makeItemTypesSearchIndex(),
+		makeRacesSearchIndex(),
 	]);
 
 main().catch((reason) => {
